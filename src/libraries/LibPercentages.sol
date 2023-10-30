a12c15f3a447b7ce11a5184eb28d7de59ce7b364
royvardhan <royvardhan.official@gmail.com>
2023-10-30 17:41:39 +0530

    feat: update contract

--- src/libraries/LibPercentages.sol
+++ src/libraries/LibPercentages.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.20;
 
 library LibPercentages {
     /// @notice Calculates the percentage of a number using basis points
